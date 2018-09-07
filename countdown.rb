c99a05341e990372bbd7faea479bf3343cb57341
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-07 08:42:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,11 @@ def countdown(seconds)
   return "HAPPY NEW YEAR!"
 end
 
-
+def countdown_with_sleep(seconds)
+  while seconds > 0 
+    puts "#{seconds} SECOND(S)!"
+    seconds -=1 
+    sleep(5)
+  end 
+  return "HAPPY NEW YEAR!"
+end 
